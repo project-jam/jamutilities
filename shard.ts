0230f0a509c64d5c9db2b71966741f1d01c5cf69
Omarplayz233 <omarplayzsounds@gmail.com>
2025-02-21 22:25:02 +0100

    removed shards :(

--- shard.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { ShardHandler } from "./src/handlers/shardHandler";
-
-const shardHandler = new ShardHandler();
-shardHandler.spawn().catch((error) => {
-  console.error("Failed to spawn shards:", error);
-  process.exit(1);
-});
